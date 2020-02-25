cfd1ad6bed090a9937d721169d6747419280e199
wencaizhang <1052642137@qq.com>
2020-02-26 00:11:49 +0800

    完善接口架构

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags, ApiProperty } from '@nestjs/swagger';
+
+class CreatePostDto {
+  @ApiProperty({ description: '博客标题', example: '博客标题' })
+  title: string
+  @ApiProperty({ description: '博客内容', example: '博客内容' })
+  content: string
+}
 
 @Controller('posts')
 export class PostsController {
 
   @Get()
-  list() {
+  @ApiTags('博客')
+  @ApiOperation({ summary: '博客列表' })
+  findAll (@Query() query) {
     return [
       { id: 1 },
       { id: 2 },
@@ -14,23 +24,31 @@ export class PostsController {
   }
 
   @Get(':id')
-  detail() {
+  @ApiTags('博客')
+  @ApiOperation({ summary: '获取博客详细信息'})
+  findOne(@Query('id') id: string) {
     return { id: 1 }
   }
 
   @Post()
-  create() {
+  @ApiTags('博客')
+  @ApiOperation({ summary: '创建博客'})
+  create(@Body() body: CreatePostDto, ) {
     return { message: '创建成功' }
   }
 
-  @Put()
-  update() {
+  @Put(':id')
+  @ApiTags('博客')
+  @ApiOperation({ summary: '更新博客'})
+  update(@Param('id') id: string, @Body() updatePostData: CreatePostDto) {
     return { message: '更新成功' }
   }
 
   @Delete(':id')
-  delete() {
+  @ApiTags('博客')
+  @ApiOperation({ summary: '删除博客'})
+  remove(@Param('id') id: string) {
     return { message: '删除成功' }
   }
-  
+
 }
