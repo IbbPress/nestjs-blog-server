1ea84629c2dd51928b42a261ca7237d398541665
wencaizhang <1052642137@qq.com>
2020-02-28 00:55:02 +0800

    引入package: class-validator class-transformer 进行数据校验

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3000
   const swaggerPath = 'api-doc'
+
+  app.useGlobalPipes(new ValidationPipe())
 
   const options = new DocumentBuilder()
     .setTitle('IbbPress 博客接口文档')
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
 
 export class CreatePostDto {
   @ApiProperty({ description: '博客标题', example: '博客标题' })
+  @IsNotEmpty({ message: '请填写标题' })
   title: string
   @ApiProperty({ description: '博客内容', example: '博客内容' })
+  @IsNotEmpty({ message: '请填写博客内容' })
   content: string
   @ApiProperty({ description: '博客作者', example: 'wencaizhang' })
   author: string
