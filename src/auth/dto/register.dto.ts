2e041d491e491be112fbab3640affbb3a8dde18e
wencaizhang <1052642137@qq.com>
2020-03-16 20:04:50 +0800

    数据类型修改

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,16 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class RegisterDto {
-  @ApiProperty({ description: '用户名', example: 'wencai' })
+  @ApiProperty({ description: '登录名', example: 'wencai' })
   username: string;
   @ApiProperty({ description: '密码', example: '123456' })
   password: string
-  @ApiProperty({ description: '真实姓名', example: 'zhang'})
-  realName: string;
+  @ApiProperty({ description: '昵称', example: 'zhang'})
+  nickName: string;
   @ApiProperty({ description: '电子邮件', example: '1052642137@qq.com'})
   email: string;
-  @ApiProperty({ description: '个人说明', example: '一个青年'})
-  desc: string;
-  @ApiProperty({ description: '头像', example: 'zhang'})
-  avator: string;
 }
