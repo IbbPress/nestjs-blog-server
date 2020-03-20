cc7975d9aa2d88776a22750e5135dd72a96d28ab
wencaizhang <1052642137@qq.com>
2020-03-20 16:21:04 +0800

    认证过期时间改为24小时

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { LocalStrategy } from './strategies/local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '600s' },
+      signOptions: { expiresIn: `${60 * 60 * 24}s` },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
